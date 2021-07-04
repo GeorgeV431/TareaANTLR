3d9dfe1642fe93555d1576ef88a9febe42078e16
Aaron <aaron.frenkel@iguanabee.com>
2021-07-04 02:49:22 -0400

    Update MyVisitor.java

--- Len -2021/src/MAIN/MyVisitor.java
+++ Len -2021/src/MAIN/MyVisitor.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import ANTLR.ParserDNDBaseVisitor;
+import ANTLR.ParserDNDParser.AsignacionContext;
 import ANTLR.ParserDNDParser.DeclaracionContext;
 import ANTLR.ParserDNDParser.PrintSentenceContext;
 import ANTLR.ParserDNDParser.Tipo_datoContext;
@@ -15,6 +16,7 @@ import ANTLR.ParserDNDParser.Tipo_datoContext;
 
 public class MyVisitor extends ParserDNDBaseVisitor<Integer> {
 	private Map<String, String>  variables = new HashMap<String, String>();
+	private Map<String, String>  variablesTipo = new HashMap<String, String>();
 	
 	
 	//--------------- Funcion Imprimir -------------//
@@ -41,19 +43,45 @@ public class MyVisitor extends ParserDNDBaseVisitor<Integer> {
 	       if (variables.containsKey(id))
 	            throw new IllegalArgumentException("Variable '" + id + "' ya fue declarada");
 	       else{
-	    	   if( ctx.valor() != null ) {
+	    	   if( ctx.asignacion() != null ) {
 	    		   
 	    		   
+	    	   }else {
+	    		   variables.put(id, value);
 	    	   }
-	    	   
 	       }
-	       
 	       
 	       return 0;
 	}
 	
 	
 	//-------------- Asignar Variables 		-----------//
+	@Override
+	public Integer visitAsignacion(AsignacionContext ctx){	        
+	       String id = ctx.nombre(0).getText();
+	       String value = "";
+	       
+	       if (variables.containsKey(id)) {
+	           if(variables.containsKey(id)) {
+	        	   
+	        	   
+	        	   
+	           }
+	    	   
+	    	   
+	       }else{
+	    	   if( ctx.operacion() != null ) {
+	    		   
+	    		   
+	    	   }
+	    	   variables.put(id, value);
+	    	   
+	       }
+	       
+	       return 0;
+	}
+	
+	
 	//-------------- Funcion Matematica 	-----------//
 	//-------------- Leer Variables 		-----------//
 	//-------------- Operar Variables 		-----------//
